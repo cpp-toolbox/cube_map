7acca1daae200782cfd150cf17ce91e8df41fc39
ccn <ccn@cuppajoeman.com>
2025-01-14 19:50:09 -0500

    feat: add support for windows

--- cube_map.cpp
+++ cube_map.cpp
@@ -1,7 +1,9 @@
 #include "cube_map.hpp"
 #include <glm/fwd.hpp>
+#include <filesystem>
 #include <string>
 #include <vector>
+#include <iostream>
 #include <functional>
 
 // this is how cube map images are connected together
@@ -22,6 +24,7 @@
 //              |         |
 //              |---------|
 //
+
 
 std::vector<glm::vec2> base_texture_coordinates = {
     {0.0f, 0.0f}, // Bottom-left
@@ -50,26 +53,16 @@ std::vector<glm::vec3> cube_vertex_positions_ = {{-1.0f, -1.0f, 1.0f},  // 0  Co
 // U: 7 6 5 5 4 7
 // D: 0 1 2 2 3 0
 
-std::string remove_trailing_slash(const std::string &path) {
-    // Check if the path is empty
-    if (path.empty()) {
-        return path;
-    }
 
-    // Copy the input path to modify it
-    std::string result = path;
-
-    // Check if the last character is a slash and remove it if necessary
-    if (result.back() == '/' || result.back() == '\\') {
-        result.pop_back();
-    }
-
-    return result;
-}
+CubeMap::CubeMap(const std::filesystem::path &cube_map_dir, const std::string &file_extension, TexturePacker &texture_packer)
+    : cube_map_dir(cube_map_dir), file_extension(file_extension), texture_packer(texture_packer),
+      top_face(create_face("up", {7, 6, 5, 5, 4, 7})), bottom_face(create_face("down", {0, 1, 2, 2, 3, 0})),
+      left_face(create_face("left", {0, 3, 7, 7, 4, 0})), right_face(create_face("right", {2, 1, 5, 5, 6, 2})),
+      front_face(create_face("front", {3, 2, 6, 6, 7, 3})), back_face(create_face("back", {1, 0, 4, 4, 5, 1})) {}
 
 IVPTexturePacked CubeMap::create_face(const std::string &face_name, const std::vector<unsigned int> &face_indices) {
     auto create_path = [&](const std::string &file_name) {
-        return remove_trailing_slash(cube_map_dir) + "/" + file_name + "." + file_extension;
+        return (cube_map_dir / (file_name + "." + file_extension)).string();
     };
 
     auto extract_face_vertices = [&](const std::vector<unsigned int> &indices) {
@@ -81,6 +74,7 @@ IVPTexturePacked CubeMap::create_face(const std::string &face_name, const std::v
     };
 
     std::string texture_path = create_path(face_name);
+    std::cout << "cubemap texpath: " << texture_path << std::endl;
     int packed_texture_index = texture_packer.get_packed_texture_index_of_texture(texture_path);
     std::vector<glm::vec2> packed_coordinates =
         texture_packer.get_packed_texture_coordinates(texture_path, base_texture_coordinates);
@@ -88,24 +82,11 @@ IVPTexturePacked CubeMap::create_face(const std::string &face_name, const std::v
     int packed_texture_bounding_box_index =
         texture_packer.get_packed_texture_bounding_box_index_of_texture(texture_path);
 
-    // this is really dumb right now, working but fix and make this whole setup better
     std::vector<unsigned int> square_indices = {0, 1, 2, 3, 4, 5};
 
     return IVPTexturePacked(square_indices, extract_face_vertices(face_indices), base_texture_coordinates,
                             packed_coordinates, packed_texture_index, packed_texture_bounding_box_index, texture_path);
 }
-
-// L: 0 3 7 7 4 0
-// F: 3 2 6 6 7 3
-// R: 2 1 5 5 6 2
-// B: 1 0 4 4 5 1
-// U: 7 6 5 5 4 7
-// D: 0 1 2 2 3 0
-CubeMap::CubeMap(const std::string &cube_map_dir, const std::string &file_extension, TexturePacker &texture_packer)
-    : cube_map_dir(cube_map_dir), file_extension(file_extension), texture_packer(texture_packer),
-      top_face(create_face("up", {7, 6, 5, 5, 4, 7})), bottom_face(create_face("down", {0, 1, 2, 2, 3, 0})),
-      left_face(create_face("left", {0, 3, 7, 7, 4, 0})), right_face(create_face("right", {2, 1, 5, 5, 6, 2})),
-      front_face(create_face("front", {3, 2, 6, 6, 7, 3})), back_face(create_face("back", {1, 0, 4, 4, 5, 1})) {}
 
 void CubeMap::regenerate() {
     top_face = create_face("up", {7, 6, 5, 5, 4, 7});
--- cube_map.hpp
+++ cube_map.hpp
@@ -1,19 +1,19 @@
 #ifndef CUBEMAP_HPP
 #define CUBEMAP_HPP
 
+#include <filesystem>
 #include <string>
 #include "sbpt_generated_includes.hpp"
 
 class CubeMap {
   public:
-    CubeMap(const std::string &cube_map_dir, const std::string &file_extension, TexturePacker &texture_packer);
+    CubeMap(const std::filesystem::path &cube_map_dir, const std::string &file_extension, TexturePacker &texture_packer);
     IVPTexturePacked create_face(const std::string &face_name, const std::vector<unsigned int> &face_indices);
 
     void regenerate();
 
     const std::string file_extension;
-    const std::string cube_map_dir;
-    // wow I can't believe I didn't know this till now, you must store as a reference or its copied (facepalm)
+    const std::filesystem::path cube_map_dir;
     TexturePacker &texture_packer;
     IVPTexturePacked top_face;
     IVPTexturePacked bottom_face;
